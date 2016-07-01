08adc981892e89435d7dbc83b7ca32bbc47e0364
Sameer87 <direct2sameer@gmail.com>
2016-07-01 14:01:15 +0530

    small changes

--- website/settings.py
+++ website/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'urlshort.apps.UrlshortConfig',
-    'music.apps.MusicConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
