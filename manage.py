ad36f93f2c85af03c72020eca0fade7d6ff24643
Vipul Maan <vipulmaan@outlook.com>
2016-10-22 15:25:55 +0530

    update manage.py

--- manage.py
+++ manage.py
@@ -8,3 +8,4 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+#I don't get what is this but i m creating a PR, how silly is this 
